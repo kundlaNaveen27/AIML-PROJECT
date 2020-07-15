1ba7a0a5f33964bafc8e902ef36ed98249c9bb25
kundlaNaveen27 <58872290+kundlaNaveen27@users.noreply.github.com>
2020-07-15 23:59:54 +0530

    Add files via upload

---
+++ Finding Donors/visuals.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+
+
+def distribution(data, transformed = False):
+    """
+    Visualization code for displaying skewed distributions of features
+    """
+    
+    # Create figure
+    fig = plt.figure(figsize = (11,5));
+
+    # Skewed feature plotting
+    for i, feature in enumerate(['capital-gain','capital-loss']):
+        ax = fig.add_subplot(1, 2, i+1)
+        ax.hist(data[feature], bins = 25, color = '#00A0A0')
+        ax.set_title("'%s' Feature Distribution"%(feature), fontsize = 14)
+        ax.set_xlabel("Value")
+        ax.set_ylabel("Number of Records")
+        ax.set_ylim((0, 2000))
+        ax.set_yticks([0, 500, 1000, 1500, 2000])
+        ax.set_yticklabels([0, 500, 1000, 1500, ">2000"])
+
+    # Plot aesthetics
+    if transformed:
+        fig.suptitle("Log-transformed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
+    else:
+        fig.suptitle("Skewed Distributions of Continuous Census Data Features", \
+            fontsize = 16, y = 1.03)
+
+    fig.tight_layout()
+    fig.show()
